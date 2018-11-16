d989d45bf510d3a5f986a9054bc574efb895d1d5
Mirandinha <karine.lm@hotmail.com>
2018-11-15 23:26:36 -0200

    Iniciando segundo repositório.

---
+++ src/TesteBancoDeDados/CategoriaTeste.java
@@ -0,0 +1,58 @@
+package TesteBancoDeDados;
+
+import java.util.List;
+import model.Categoria;
+import model.DAOCategoria;
+
+/**
+ * @author Mirandinha
+ */
+public class CategoriaTeste {
+
+//    public static void main(String[] args) {
+
+//         ================ Validação do método adiciona categoria no BD. ================
+//        DAOCategoria conexaoCategoria = new DAOCategoria();
+//        Categoria categoria1 = new Categoria("Teste1501");
+//        if (conexaoCategoria.adicionaCategoria(categoria1)) {
+//            System.out.println("Categoria adicionada com sucesso");
+//        } else {
+//            System.out.println("Categoria NÃO adicionada");
+//        }
+        // ================ Validação do método atualiza categoria no BD. ================
+//        DAOCategoria conexaoCategoria = new DAOCategoria();
+//        Categoria categoria1 = new Categoria();
+//        categoria1.setIdCategoria(1);
+//        categoria1.setDescricaoBD("Oi");
+//        if (conexaoCategoria.atualizaCategoria(categoria1)) {
+//            System.out.println("Categoria atualizada com sucesso");
+//        } else {
+//            System.out.println("Categoria NÃO atualizada");
+//        }
+        // ================ Validação do método remove categoria no BD. ================
+//        DAOCategoria conexaoCategoria = new DAOCategoria();
+//        Categoria categoria = new Categoria();
+//        categoria.setIdCategoria(1);
+//        if (conexaoCategoria.removeCategoria(categoria)) {
+//            System.out.println("Categoria removida com sucesso");
+//        } else {
+//            System.out.println("Categoria NÃO removida");
+//        }
+        //================ Validação do método que retorna uma lista com todos as categorias do BD. ================
+//        DAOCategoria categoriaDB = new DAOCategoria();
+//        List<Categoria> listaDeCategorias = categoriaDB.retornaListaDeCategorias();
+//        if (listaDeCategorias != null) {
+//            System.out.println("Listagem das categorias: ");
+//            for (int i = 0; i < listaDeCategorias.size(); i++) {
+//                listaDeCategorias.get(i).exibeDadosCategoria();
+//            }
+//        } else {
+//            System.out.println("Lista nula!");
+//        }
+//================ Validação do método que retorna uma categoria do BD. ================
+//        DAOCategoria conexaoCategoria = new DAOCategoria();
+//        Categoria categoria = new Categoria();
+//        categoria.setIdCategoria(5);
+//        conexaoCategoria.retornaUmaCategoria(categoria).exibeDadosCategoria();
+//    }
+}
