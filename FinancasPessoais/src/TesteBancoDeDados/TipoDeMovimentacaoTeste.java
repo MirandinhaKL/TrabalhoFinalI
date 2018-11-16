c05a15da143b23ae2e02379b6da46e8ba6c49100
Karine Miranda <karine.lm@hotmail.com>
2018-11-15 23:46:26 -0200

    Inicial - 15/11/18 - Tentativa 2
    Tela Login e Principal carregando corretamente.
    Classes DAO Tipo de Movimentação e Categoria funcionando 100%.
    Classe DAO Movimentação NÃO funciona!

---
+++ FinancasPessoais/src/TesteBancoDeDados/TipoDeMovimentacaoTeste.java
@@ -0,0 +1,58 @@
+package TesteBancoDeDados;
+
+import java.util.List;
+import model.TipoDeMovimentacao;
+import model.DAOTipoDeMovimentacao;
+
+/**
+ * @author Mirandinha
+ */
+public class TipoDeMovimentacaoTeste {
+
+//    public static void main(String[] args) {
+
+        //================ Validação do método adiciona TipoDeMovimentação no BD. ================
+//        DAOTipoDeMovimentacao conexaoTipoDeMovimentacao = new DAOTipoDeMovimentacao();
+//        TipoDeMovimentacao tipo = new TipoDeMovimentacao("Teste1511");
+//        if (conexaoTipoDeMovimentacao.adicionaTipoDeMovimentacao(tipo)) {
+//            System.out.println("Tipo de movimentação adicionado com sucesso!");
+//        } else {
+//            System.out.println("Tipo de movimentação NÃO adicionado");
+//        }
+// ================ Validação do método atualiza TipoDeMovimentação no BD. ================
+//        DAOTipoDeMovimentacao conexaoTipoDeMovimentacao = new DAOTipoDeMovimentacao();
+//        TipoDeMovimentacao tipo = new TipoDeMovimentacao();
+//        tipo.setIdTipoMovimentacao(5);
+//        tipo.setDescricaoBD("Olá");
+//        if (conexaoTipoDeMovimentacao.atualizaTipoDeMovimentacao(tipo)) {
+//            System.out.println("Tipo de movimentação atualizado com sucesso!");
+//        } else {
+//            System.out.println("Tipo de movimentação NÃO atualizado!");
+//        }
+// ================ Validação do método remove TipoDeMovimentação no BD. ================
+//        DAOTipoDeMovimentacao conexaoTipoDeMovimentacao = new DAOTipoDeMovimentacao();
+//        TipoDeMovimentacao tipo = new TipoDeMovimentacao();
+//        tipo.setIdTipoMovimentacao(8);
+//        if (conexaoTipoDeMovimentacao.removeTipoDeMovimentação(tipo)) {
+//            System.out.println("Tipo de movimentação removida com sucesso!");
+//        } else {
+//            System.out.println("Tipo de movimentação NÃO foi removida.");
+//        }
+//================ Validação do método que retorna uma lista com todos os tipos de movimentação do BD. ================
+//        DAOTipoDeMovimentacao dataBaseTipoDeMovimentacao = new DAOTipoDeMovimentacao();
+//        List<TipoDeMovimentacao> tiposDeMovDoBD = dataBaseTipoDeMovimentacao.retornaListaDosTiposDeMovimentaoes();
+//        if (tiposDeMovDoBD != null) {
+//            System.out.println("Listagem dos tipos de movimentações: ");
+//            for (int i = 0; i < tiposDeMovDoBD.size(); i++) {
+//                tiposDeMovDoBD.get(i).exibeTiposDeMovimetacoes();
+//            }
+//        } else {
+//            System.out.println("Lista nula");
+//        }
+//================ Validação do método que retorna um tipo de movimentação do BD. ================
+//        DAOTipoDeMovimentacao conexaoTipoDeMovimentacao = new DAOTipoDeMovimentacao();
+//        TipoDeMovimentacao tipo = new TipoDeMovimentacao();
+//        tipo.setIdTipoMovimentacao(9);
+//        conexaoTipoDeMovimentacao.retornaUmTipo(tipo).exibeTiposDeMovimetacoes();
+//    }
+}
---
+++ FinancasPessoais/src/model/DAOCategoria.java
@@ -0,0 +1,125 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mirandinha
+ */
+public class DAOCategoria {
+
+    private Connection conexao;
+
+    /**
+     * Classe construtora que cria a conexão com o banco de dados.
+     */
+    public DAOCategoria() {
+        this.conexao = new ConexaoBancoDeDados().getConexao();
+    }
+
+    public boolean adicionaCategoria(Categoria novaCategoria) {
+        String sql = "insert into categoria(descricao) values(?);";
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            declaracao.setString(1, novaCategoria.getDescricao());
+            declaracao.execute();
+            declaracao.close();
+            conexao.close();
+            return true;
+        } catch (SQLException excecao) {
+            excecao.getMessage();
+            return false;
+        }
+    }
+
+    public boolean atualizaCategoria(Categoria categoria) {
+        String sql = "update categoria set descricao = ? where id = ?;";
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            declaracao.setString(1, categoria.getDescricaoBD());
+            declaracao.setInt(2, categoria.getIdCategoria());
+            declaracao.execute();
+            declaracao.close();;
+            conexao.close();
+            return true;
+        } catch (Exception excecao) {
+            excecao.getMessage();
+            return false;
+        }
+    }
+
+    public boolean removeCategoria(Categoria categoria) {
+        String sql = "delete from categoria where id = ?;";
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            declaracao.setInt(1, categoria.getIdCategoria());
+            declaracao.execute();
+            declaracao.close();
+            conexao.close();
+            return true;
+        } catch (SQLException excecao) {
+            excecao.getMessage();
+            return false;
+        }
+    }
+
+    public boolean removeTodasAsCategorias() {
+        String sql = "delete from categoria;";
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            declaracao.execute();
+            declaracao.close();
+            conexao.close();
+            return true;
+        } catch (SQLException excecao) {
+            excecao.getMessage();
+            return false;
+        }
+    }
+
+    public List<Categoria> retornaListaDeCategorias() {
+        List<Categoria> listaDeCategorias = new ArrayList<>();
+        String sql = "select * from categoria;";
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            ResultSet consultaBD = declaracao.executeQuery();
+            while (consultaBD.next()) {
+                Categoria itemCategoria = new Categoria();
+                itemCategoria.setIdCategoria(consultaBD.getInt("id"));
+                itemCategoria.setDescricaoBD(consultaBD.getString("descricao"));
+                listaDeCategorias.add(itemCategoria);
+            }
+            declaracao.close();
+            consultaBD.close();;
+            conexao.close();
+        } catch (Exception excecao) {
+            excecao.printStackTrace();;
+            System.out.println("Erro! Lista não retornada!");
+            return null;
+        }
+        return listaDeCategorias;
+    }
+    
+    public Categoria retornaUmaCategoria(Categoria categoria){
+        String sql = "select * from categoria where id=?;";
+        Categoria retornaCategoria = new Categoria();
+        try {
+            PreparedStatement declaracao = conexao.prepareStatement(sql);
+            declaracao.setInt(1, categoria.getIdCategoria());
+            ResultSet consultaBD = declaracao.executeQuery();
+            System.out.println("categoira retoranda");
+            if (consultaBD.next()) {
+                categoria.setDescricaoBD(consultaBD.getString("descricao"));
+                retornaCategoria = categoria;
+            }
+        } catch (Exception excecao) {
+            excecao.getMessage();
+        }
+        return retornaCategoria;
+    }
+}
